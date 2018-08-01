44d867f7666d396a2342edc016258140be4640f9
Şahin Sarıhan <sahinsarihan@std.sehir.edu.tr>
2018-08-01 19:07:25 +0300

    variant_calling_fix

--- run_pipeline_variant_calling.py
+++ run_pipeline_variant_calling.py
@@ -2,28 +2,39 @@ import variant_calling
 from glob import glob
 from os import chdir
 
-def onVCaller(working_directory, var_maptype, var_variantcaller, germline_path, threads):
+
+def call_variant_caller(working_directory, tumor_bam, tumor_interval, germline_bam, germline_interval, var_maptype, var_variantcaller, threads):
     wd = working_directory
     if wd[-1] == "/" or wd[-1] == "\\":
         wd = wd[:-1]
     mt = var_maptype
     vc = var_variantcaller
-    gm = germline_path
+    gm = germline_bam
     if gm[-1] == "/" or gm[-1] == "\\":
         gm = gm[:-1]
     th = threads
-    chdir(gm + "/" + mt)
-    gm_bam = glob("GATK_PRIR*.bam")
-    gm_interval = glob("*realign_target.intervals")
-    chdir(wd + "/" + mt)
-    bam = gm + "/" + mt + "/" + gm_bam[0]
-    interval = gm + "/" + mt + "/" + gm_interval[0]
+    gm_interval = germline_interval
+    if gm[-1] == "/" or gm[-1] == "\\":
+        gm = gm[:-1]
+    bam = tumor_bam
+    bam_interval = tumor_interval
     if vc == "Mutect2":
-        pipeline2 = variant_calling.VariantCall(variant_caller=vc, thrds=th, map_type=mt, germline_bam=bam,
-                                                germline_realign=interval, wd=wd)
+        pipeline2 = variant_calling.VariantCall(variant_caller=vc, thrds=th, map_type=mt, germline_bam=gm,
+                                                germline_interval=gm_interval, wd=wd, tumor_bam=bam,
+                                                tumor_interval=bam_interval)
     else:
-        pipeline2 = variant_calling.VariantCall(variant_caller=vc, thrds=th, map_type=mt, germline_bam=bam,
-                                                germline_realign=None, wd=wd)
+        pipeline2 = variant_calling.VariantCall(variant_caller=vc, thrds=th, map_type=mt, germline_bam=gm,
+                                                germline_interval=None, wd=wd, tumor_bam=bam,
+                                                tumor_interval=bam_interval)
     pipeline2_success = pipeline2.run_pipeline()
 
     return pipeline2_success
+
+
+
+call_variant_caller(var_variantcaller="Mutect2", threads=6, var_maptype="Bwa",
+                    germline_bam="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa_mapped/OutputBAM_NOB01_last_Bwa.bam",
+                    germline_interval="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa_mapped/realign_target.intervals",
+                    working_directory="/home/bioinformaticslab/Desktop/GitHub_Repos/Genomics_Pipeline_Test/test_files/Bwa_test1/GatkPreProcess",
+                    tumor_bam="Output_GATK_Bwa_NB17.bam", tumor_interval="realign_target.intervals")
+
--- variant_calling.py
+++ variant_calling.py
@@ -9,25 +9,23 @@ import shutil
 
 class VariantCall(object):
 
-    def __init__(self, variant_caller, thrds, map_type, germline_bam, germline_realign, wd):
+    def __init__(self, variant_caller, thrds, map_type, germline_bam, germline_interval, wd, tumor_bam, tumor_interval):
         self.get_paths = GetPaths()
-        self.main_directory = wd
-        self.folder_directory = wd + "/" + map_type
-        self.working_directory = wd + "/" + map_type + "/GatkPreProcess"
+        self.working_directory = wd
+        # self.folder_directory = wd + "/" + map_type
+        # self.working_directory = wd + "/" + map_type + "/GatkPreProcess"
         os.chdir(self.working_directory)
         print(self.working_directory)
         self.v_caller = variant_caller
         self.threads = str(thrds)
         self.map_type = map_type
         self.ref_dir = self.get_paths.ref_dir + "hg19_bundle/ucsc.hg19.fasta"
-        tumor_bam = glob.glob("Output_*.bam")
-        tumor_realign = glob.glob("realign_target.intervals")
-        self.tumor_bam = self.working_directory + "/" + tumor_bam[0]
+        self.tumor_bam = tumor_bam
         self.germline_bam = germline_bam
         if variant_caller == "Mutect2":
-            self.tumor_realign = self.working_directory + "/" + tumor_realign[0]
-            self.germline_realign = germline_realign
-            self.realign_target = self.tumor_realign + " " + self.germline_realign
+            self.tumor_interval = tumor_interval
+            self.germline_interval = germline_interval
+            self.realign_target = self.tumor_interval + " " + self.germline_interval
         self.output_vcf = variant_caller + "_ouput.vcf"
 
     def run_pipeline(self):
@@ -39,6 +37,8 @@ class VariantCall(object):
             self.varscan_caller()
             files = glob.glob("*.vcf*")
             self.create_folder(files)
+        else:
+            return False
         return True
 
     def mutect_caller(self):
@@ -46,15 +46,15 @@ class VariantCall(object):
         command = "java -jar " + self.get_paths.gatk_path + " -T MuTect2 " + nct + " -R " + self.ref_dir + \
                   " -I:tumor " + self.tumor_bam + " -I:normal " + self.germline_bam + \
                   " --dbsnp " + self.get_paths.dbsnp + " --cosmic " + self.get_paths.cosmic + \
-                  " -L " + self.tumor_realign + " -L " + self.germline_realign + " -o " + self.output_vcf
+                  " -L " + self.tumor_interval + " -L " + self.germline_interval + " -o " + self.output_vcf
         print(command)
-        log_command(command, "mutect_caller", self.threads)
+        log_command(command, "Mutect2", self.threads, "Variant Calling")
 
     def varscan_caller_step1(self):
         command = "samtools mpileup -f " + self.ref_dir + " -q 1 -B " + self.tumor_bam + " " + \
                   self.germline_bam + " > intermediate_mpileup.pileup"
         print(command)
-        log_command(command, "varscan_caller_step1", self.threads)
+        log_command(command, "Varscan Step 1", self.threads, "Variant Calling")
         intermediate_mpileup = glob.glob("intermediate_mpileup.pileup")
 
         return intermediate_mpileup[0]
@@ -67,7 +67,7 @@ class VariantCall(object):
                   "--min-freq-for-hom 0.75 --normal-purity 1.0 --tumor-purity 1.00 --p-value 0.99 " \
                   "--somatic-p-value 0.05 " + "--strand-filter 0 --output-vcf"
         print(command)
-        log_command(command, "varscan_caller_step2", self.threads)
+        log_command(command, "Varscan Step 2", self.threads, "Variant Calling")
         intermediate_varscan_somatic = glob.glob("output.basename*")
 
         return intermediate_varscan_somatic
@@ -77,7 +77,7 @@ class VariantCall(object):
         for somatic in intermediate_varscan_somatic:
             command = "java -jar " + self.get_paths.varscan_path + " processSomatic " + somatic + \
                       " --min-tumor-freq 0.10 --max-normal-freq 0.05 --p-value 0.07"
-            log_command(command, "varscan_caller_step3", self.threads)
+            log_command(command, "Varscan Step 3", self.threads, "Variant Calling")
         return glob.glob("output.basename*")
 
     def varscan_caller(self):
@@ -87,17 +87,23 @@ class VariantCall(object):
         print(step3)
 
     def create_folder(self, all_files):
-        mk_dir = self.folder_directory + "/" + self.v_caller
+        up_dir = str(self.working_directory).split("/")[:-1]
+        mk_dir = "/".join(up_dir) + "/" + self.v_caller
+        print("**************MKDIR 1 ***************")
         os.mkdir(mk_dir)
+        print(mk_dir)
+        print("**************MKDIR 2 ***************")
         for file in all_files:
             if file[-2:] != "gz":
                 print(file)
                 shutil.move(self.working_directory + "/" + file, mk_dir + "/" + file)
 
-
+#variant_caller, thrds, map_type, germline_bam, germline_interval, wd, tumor_bam, tumor_interval
 if __name__ == "__main__":
-    mutectvcf = VariantCall(variant_caller="Varscan", thrds=3, map_type="Bwa",
-                                            germline_bam="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa/OutputBAM_NOB01_last_Bwa.bam",
-                                            germline_realign="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa/realign_target.intervals",
-                            wd="/home/bioinformaticslab/Desktop/GitHub_Repos/Genomics_Pipeline_Test/test_files")
+    mutectvcf = VariantCall(variant_caller="Mutect2", thrds=6, map_type="Bwa",
+                            germline_bam="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa_mapped/OutputBAM_NOB01_last_Bwa.bam",
+                            germline_interval="/home/bioinformaticslab/Desktop/AMBRY/Sample_NOB01_GermlineDNA/Bwa_mapped/realign_target.intervals",
+                            wd="/home/bioinformaticslab/Desktop/GitHub_Repos/Genomics_Pipeline_Test/test_files/Bwa_test1/GatkPreProcess",
+                            tumor_bam="Output_GATK_Bwa_NB17.bam",
+                            tumor_interval="realign_target.intervals")
     mutectvcf.run_pipeline()
