2e095b4d098be4f92c79fa986dc160eca890a8d7
Şahin Sarıhan <sahinsarihan@std.sehir.edu.tr>
2018-10-17 15:44:38 +0300

    edit-path

--- paths.py
+++ paths.py
@@ -1,12 +1,12 @@
 
 class GetPaths(object):
     def __init__(self):
-        self.picard_path = "/home/bioinformaticslab/Desktop/GenomicsWorks/picard.jar"
-        self.gatk_path = "/home/bioinformaticslab/Desktop/GenomicsWorks/GenomeAnalysisTK.jar"
-        self.ref_dir = "/home/bioinformaticslab/Desktop/GenomicsWorks/ref_genome_indexes/"
-        self.varscan_path = "/home/bioinformaticslab/Desktop/GenomicsWorks/VarScan.v2.3.9.jar"
-        self.dbsnp = "/home/bioinformaticslab/Desktop/GenomicsWorks/ref_genome_indexes/hg19_bundle/dbsnp_138.hg19.vcf"
-        self.mills_indel = "/home/bioinformaticslab/Desktop/GenomicsWorks/ref_genome_indexes/hg19_bundle/Mills_and_1000G_gold_standard.indels.hg19.vcf"
-        self.fastqc = "/home/bioinformaticslab/Desktop/GenomicsWorks/FastQC/fastqc"
-        self.cosmic = ""
-        self.annovar = "/home/bioinformaticslab/Desktop/GenomicsWorks/annovar/"
+        self.picard_path = "edit-path/picard.jar"
+        self.gatk_path = "edit-path/GenomeAnalysisTK.jar"
+        self.ref_dir = "edit-path/ref_genome_indexes/"
+        self.varscan_path = "edit-path/VarScan.v2.3.9.jar"
+        self.dbsnp = "edit-path/dbsnp_138.hg19.vcf"
+        self.mills_indel = "edit-path/Mills_and_1000G_gold_standard.indels.hg19.vcf"
+        self.fastqc = "edit-path/fastqc"
+        self.cosmic = "edit-path/cosmic"
+        self.annovar = "edit-path/annovar/"
